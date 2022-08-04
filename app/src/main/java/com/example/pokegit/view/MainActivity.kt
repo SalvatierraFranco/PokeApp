4203e5a9b77d2773f943101d508a051699895d79
Franco <franco.salvatierradvp@gmail.com>
2022-08-04 13:26:06 -0300

    Se crea splash para el inicio de la APP y tambien se crea arquitectura MVVM

--- app/src/main/java/com/example/pokegit/view/MainActivity.kt
+++ app/src/main/java/com/example/pokegit/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pokegit
+package com.example.pokegit.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokegit.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
