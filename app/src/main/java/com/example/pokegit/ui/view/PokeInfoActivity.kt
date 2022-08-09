feff1b05cb7e4e3c367574f310c4c2782e04046e
Franco <franco.salvatierradvp@gmail.com>
2022-08-09 17:51:36 -0300

    Se finaliza código UI para mostrar los detalles de item seleccionado en el RecyclerView

---
+++ app/src/main/java/com/example/pokegit/ui/view/PokeInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokegit.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokegit.R
+
+class PokeInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poke_info)
+    }
+}
