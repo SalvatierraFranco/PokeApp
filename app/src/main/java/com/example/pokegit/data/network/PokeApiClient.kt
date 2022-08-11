fbe7030acaa7f46545f14f7694c5509e5da6f2a0
Franco <franco.salvatierradvp@gmail.com>
2022-08-11 15:09:29 -0300

    Se agrega funcionalidad a RecyclerView para acceder a los detalles de cada item

--- app/src/main/java/com/example/pokegit/data/network/PokeApiClient.kt
+++ app/src/main/java/com/example/pokegit/data/network/PokeApiClient.kt
@@ -1,8 +1,10 @@
 package com.example.pokegit.data.network
 
+import com.example.pokegit.data.model.PokeInfo
 import com.example.pokegit.data.model.PokeResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PokeApiClient {
@@ -14,4 +16,7 @@ interface PokeApiClient {
         @Query("offset") offset: Int,
         @Query("limit") limit: Int
     ): Response<PokeResponse>
+
+    @GET("pokemon/{id}")
+    suspend fun getPokeInfo(@Path("id")id: Int): Response<PokeInfo>
 }
--- app/src/main/java/com/example/pokegit/ui/recycler/PokeViewHolder.kt
+++ app/src/main/java/com/example/pokegit/ui/recycler/PokeViewHolder.kt
@@ -1,8 +1,13 @@
 package com.example.pokegit.ui.recycler
 
+import android.content.Intent
 import android.view.View
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokegit.data.model.PokeInfo
+import com.example.pokegit.data.model.PokeProvider
 import com.example.pokegit.databinding.ItemPokemonBinding
+import com.example.pokegit.ui.view.PokeInfoActivity
 import com.squareup.picasso.Picasso
 
 class PokeViewHolder(view: View): RecyclerView.ViewHolder(view) {
@@ -12,5 +17,18 @@ class PokeViewHolder(view: View): RecyclerView.ViewHolder(view) {
         Picasso.get()
             .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png")
             .into(binding.ivPokeItem)
+    }
+
+    init {
+        view.setOnClickListener { v ->
+            var position = adapterPosition
+            var intent = Intent(v.context, PokeInfoActivity::class.java).apply {
+                putExtra("frontPic", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${position+1}.png")
+                putExtra("id", position+1)
+                putExtra("url", PokeProvider.pokemonList[position].url)
+            }
+            startActivity(v.context, intent, null)
+        }
+
     }
 }
